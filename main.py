2176eda10c2c2cce762f5f40ffcf8ad279be9e0f
BHImran <imran689690@gmail.com>
2024-03-29 19:29:50 +0600

    my frist commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a=int(input("enter the number :"));
+b=int(input("enter tne number:"));
+print(a+b);
+
+
+num1=float(input("enter the float number :"));
+num2=float(input("enter the float number :"));
+print(num1+num1);
+
+
+x=eval(input("enter number :"));
+y=eval(input("enter number :"));
+print(x+y);
